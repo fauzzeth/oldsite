15d00b0ee681b908121e2694043704e4ee8cd6c3
artem teslya <fauzzeth@ukr.net>
2023-03-04 15:39:41 +0200

    Update sorta.js
    Signed-off-by: artem teslya <fauzzeth@ukr.net>

--- js/sorta.js
+++ js/sorta.js
@@ -1,4 +1,4 @@
-var justshadow = "FreeMoney";
+var justshadow = "probel";
 console.warn("Ну и зачем ты сюда полез?");
 console.log("Окей, промокод находиться в переменной justshadow");
 console.log("Учи джаваскрипт");
